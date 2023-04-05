1de857f7501ed1e941050a624c4e11d6f63f465f
Caspet04 <caspet@edu.umea.se>
2023-04-05 15:32:37 +0200

    feat: implemented everything

--- oop/factory/implementations.ts
+++ oop/factory/implementations.ts
@@ -1,89 +1,293 @@
 import { Factory, Item, Machine, Part, Person } from "./interfaces";
 
-export class Chad implements Person {
-  name: string;
+export type PartNameCollection = Record<Part["name"], number>;
+export type UsePartResult =
+    | {
+          success: true;
+      }
+    | {
+          success: false;
+          missing_parts: PartNameCollection;
+      };
 
-  constructor(name: string) {
-    this.name = name;
-  }
-
-  interact(item: Item): boolean {
-    console.log("im about to try this: " + item.constructor.name);
-    return item.use();
-  }
+export function part_name_collection_to_string(part_name_collection: PartNameCollection): string {
+    return Object.entries(part_name_collection)
+        .map(([name, quantity]) => `${quantity}x ${name}`)
+        .join(", ");
 }
 
-export class Plastic implements Part {
-  description: string = "hard plastic";
-  name: string = "k3";
-}
+export function try_to_use_parts(
+    parts: Part[],
+    required_part_names: PartNameCollection
+): UsePartResult {
+    const used_parts: Part[] = [];
 
-export class Paint implements Part {
-  description: string = "nice finish theme";
-  name: string = "moomin";
-}
+    const part_names = Object.keys(required_part_names);
+    let success = true;
 
-export class Molder implements Machine {
-  parts: Plastic[] = Array.from({ length: 100 }, () => new Plastic());
-  operator: Person | undefined;
-  occupy(operator: Person): boolean {
-    throw Error("Unimplemented function");
-    // TODO: seat the operator if seat isnt taken.
-    // return success status based on if the operator could sit or not.
-  }
-  assemble(item: Disc): boolean {
-    throw Error("Unimplemented function");
-    // TODO: proceed the assembly if an operator is available
+    for (let i = 0; i < part_names.length; i++) {
+        while (required_part_names[part_names[i]] > 0) {
+            const result = parts.findIndex((part) => part.name == part_names[i]);
 
-    // TODO: take the parts (if they exist in the machine) and perform the assembly
-    // Assembly means assembling the item, adding the parts to the item.
-  }
-}
+            if (result == -1) {
+                success = false;
+                break;
+            }
 
-export class Painter implements Machine {
-  parts: Paint[] = Array.from({ length: 100 }, () => new Paint());
-  operator: Person | undefined;
-  occupy(operator: Person): boolean {
-    throw Error("Unimplemented function");
-  }
-  assemble(item: Disc): boolean {
-    throw Error("Unimplemented function");
-  }
-}
+            required_part_names[part_names[i]] -= 1;
+            used_parts.push(...parts.splice(result, 1));
+        }
 
-export class DiscFactory implements Factory {
-  assembly: Machine[] = [];
-
-  constructor() {
-    // TODO: Add the machines to the assembly
-    // TODO: Occupy the machines with workers.
-
-    const molder = new Molder();
-    const painter = new Painter();
-    molder.occupy(new Chad("billy"));
-    painter.occupy(new Chad("willy"));
-    this.assembly.push(molder);
-    this.assembly.push(painter);
-  }
-
-  produce(): Item {
-    let disc = new Disc();
-    // TODO: pass the item through the assembly line
-    // If the assebmly doesnt fail along the way, mark the item as complete
-    return disc;
-  }
-}
-
-export class Disc implements Item {
-  parts: Part[] = [];
-  complete: boolean = false;
-  use(): boolean {
-    if (!this.complete) {
-      console.log("cant throw an unfinished disc");
-      return false;
-    } else {
-      console.log("sick throw");
-      return true;
+        if (required_part_names[part_names[i]] == 0) {
+            delete required_part_names[part_names[i]];
+        }
     }
-  }
+
+    if (!success) {
+        parts.push(...used_parts);
+        return {
+            success: false,
+            missing_parts: required_part_names,
+        };
+    }
+
+    return { success: true };
+}
+
+export class Operator implements Person {
+    name: string;
+
+    public constructor(name: string) {
+        this.name = name;
+    }
+
+    public interact(item: Item): boolean {
+        return item.use();
+    }
+}
+
+// There is a subtle difference in `readonly name: string = 'Text'` and
+// `readonly name = 'Text'` and that is that the latter is a literal instead
+// of a string. This means that an instance or child class of IronPlate can
+// only have the name 'Iron Plate' otherwise it would violate the type contract.
+// This means that if a machine requires IronPlate it can only accept IronPlate or
+// equal classes.
+export class IronPlate implements Part {
+    public readonly description = "A plate of iron";
+    public readonly name = "Iron Plate";
+}
+
+export class IronGearWheel implements Part {
+    public readonly description = "A gear made out of iron";
+    public readonly name = "Iron Gear Wheel";
+}
+
+export class SteelPlate implements Part {
+    public readonly description = "A plate of steel";
+    public readonly name = "Steel Plate";
+}
+
+export class Pipe implements Part {
+    public readonly description = "A pipe made out of iron";
+    public readonly name = "Pipe";
+}
+
+export class Engine implements Item {
+    public parts: Part[] = [];
+    public complete: boolean = false;
+
+    public use(): boolean {
+        return this.complete;
+    }
+}
+
+export class IronGearWheelPress implements Machine {
+    public parts: Part[];
+    private operator?: Operator;
+
+    public constructor(parts: Part[]) {
+        this.parts = parts;
+    }
+
+    public occupy(operator: Operator): boolean {
+        if (this.operator != null) return false;
+
+        this.operator = operator;
+        return true;
+    }
+
+    public assemble(item: Item): boolean {
+        if (this.operator == null) {
+            console.log("IronGearWheelPress missing operator");
+            return false;
+        }
+
+        const result = try_to_use_parts(this.parts, { "Iron Plate": 2 });
+        if (!result.success) {
+            console.log(
+                `${
+                    this.operator.name
+                } reported IronGearWheelPress missing parts: ${part_name_collection_to_string(
+                    result.missing_parts
+                )}`
+            );
+            return false;
+        }
+
+        item.parts.push(new IronGearWheel());
+        return true;
+    }
+}
+
+export class PipeMaker implements Machine {
+    public parts: Part[];
+    private operator?: Operator;
+
+    public constructor(parts: Part[]) {
+        this.parts = parts;
+    }
+
+    public occupy(operator: Operator): boolean {
+        if (this.operator != null) return false;
+
+        this.operator = operator;
+        return true;
+    }
+
+    public assemble(item: Item): boolean {
+        if (this.operator == null) {
+            console.log("PipeMaker missing operator");
+            return false;
+        }
+
+        const result = try_to_use_parts(this.parts, { "Iron Plate": 1 });
+        if (!result.success) {
+            console.log(
+                `${
+                    this.operator.name
+                } reported PipeMaker missing parts: ${part_name_collection_to_string(
+                    result.missing_parts
+                )}`
+            );
+            return false;
+        }
+
+        item.parts.push(new Pipe());
+        return true;
+    }
+}
+
+export class SteelWelder implements Machine {
+    public parts: Part[];
+    private operator?: Operator;
+
+    public constructor(parts: Part[]) {
+        this.parts = parts;
+    }
+
+    public occupy(operator: Operator): boolean {
+        if (this.operator != null) return false;
+
+        this.operator = operator;
+        return true;
+    }
+
+    public assemble(item: Item): boolean {
+        if (this.operator == null) {
+            console.log("PipeMaker missing operator");
+            return false;
+        }
+
+        const result = try_to_use_parts(this.parts, { "Steel Plate": 1 });
+        if (!result.success) {
+            console.log(
+                `${
+                    this.operator.name
+                } reported PipeMaker missing parts: ${part_name_collection_to_string(
+                    result.missing_parts
+                )}`
+            );
+            return false;
+        }
+
+        item.parts.push(new SteelPlate());
+        return true;
+    }
+}
+
+export class EngineAssembler implements Machine {
+    public parts: Part[];
+    private operator?: Operator;
+
+    public constructor(parts: Part[]) {
+        this.parts = parts;
+    }
+
+    public occupy(operator: Operator): boolean {
+        if (this.operator != null) return false;
+
+        this.operator = operator;
+        return true;
+    }
+
+    public assemble(item: Engine): boolean {
+        if (this.operator == null) {
+            console.log("EngineAssembler missing operator");
+            return false;
+        }
+
+        const result = try_to_use_parts(item.parts, {
+            "Steel Plate": 1,
+            "Iron Gear Wheel": 1,
+            Pipe: 2,
+        });
+        if (!result.success) {
+            console.log(
+                `${
+                    this.operator.name
+                } reported EngineAssembler missing parts: ${part_name_collection_to_string(
+                    result.missing_parts
+                )}`
+            );
+            return false;
+        }
+
+        return true;
+    }
+}
+
+export class EngineFactory implements Factory {
+    public readonly assembly: Machine[];
+    private parts: Part[] = [];
+
+    public constructor() {
+        this.assembly = [
+            new IronGearWheelPress(this.parts),
+            new PipeMaker(this.parts),
+            new PipeMaker(this.parts),
+            new SteelWelder(this.parts),
+            new EngineAssembler(this.parts),
+        ];
+
+        this.assembly.forEach((machine, index) =>
+            machine.occupy(new Operator(`Operator ${index + 1}`))
+        );
+    }
+
+    public add_part(...part: Part[]) {
+        this.parts.push(...part);
+    }
+
+    public produce(): Engine {
+        const engine = new Engine();
+
+        for (let i = 0; i < this.assembly.length; i++) {
+            if (!this.assembly[i].assemble(engine)) {
+                return engine;
+            }
+        }
+
+        engine.complete = true;
+        return engine;
+    }
 }
