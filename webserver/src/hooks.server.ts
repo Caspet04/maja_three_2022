f25525ab813eb675d83925a41cea4be808fe595a
Caspet04 <caspet@edu.umea.se>
2023-04-18 09:55:09 +0200

    feat!: removed example skeleton

--- webserver/src/hooks.server.ts
+++ webserver/src/hooks.server.ts
@@ -1,29 +1,29 @@
 import type { Handle } from "@sveltejs/kit";
-import { database, init_ssr } from "$lib/ssr";
+import { database, init_ssr } from "$lib/database";
 
 init_ssr();
 
 // handle runs for every request to the server
 export const handle: Handle = async ({ event, resolve }) => {
-  const session = event.cookies.get("session");
+    const session = event.cookies.get("session");
 
-  if (session) {
-    let result = await database.user.findUnique({ where: { session } });
-    if (result?.session) {
-      event.locals.session = result.session;
+    if (session) {
+        let result = await database.user.findUnique({ where: { session } });
+        if (result?.session) {
+            event.locals.session = result.session;
+        }
     }
-  }
 
-  if (event.request.method === "OPTIONS") {
-    return new Response(null, {
-      headers: {
-        "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE",
-        "Access-Control-Allow-Origin": "*",
-      },
-    });
-  }
+    if (event.request.method === "OPTIONS") {
+        return new Response(null, {
+            headers: {
+                "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE",
+                "Access-Control-Allow-Origin": "*",
+            },
+        });
+    }
 
-  /* 
+    /* 
   
     The following line is a yolo from a security perspective.
   
@@ -33,7 +33,7 @@ export const handle: Handle = async ({ event, resolve }) => {
   
   */
 
-  const response = await resolve(event);
-  response.headers.append("Access-Control-Allow-Origin", `*`);
-  return response;
+    const response = await resolve(event);
+    response.headers.append("Access-Control-Allow-Origin", `*`);
+    return response;
 };
--- webserver/src/routes/(authenticated)/settings/+page.server.ts
+++ webserver/src/routes/(authenticated)/settings/+page.server.ts
@@ -1,35 +1,35 @@
 import { fail, redirect } from "@sveltejs/kit";
 import type { Actions } from "./$types";
-import { database } from "$lib/ssr";
+import { database } from "$lib/database";
 
 export const actions: Actions = {
-  logout: async ({ request, locals, cookies }) => {
-    const form = await request.formData();
+    logout: async ({ request, locals, cookies }) => {
+        const form = await request.formData();
 
-    // TODO: Implement register
-    // Check if ustername already exist etc.
-    cookies.delete("session");
-    throw redirect(302, "/login");
-  },
-  deleteaccount: async ({ request, locals, cookies }) => {
-    // TODO: Implement delete account
-    // Check if username already exist etc.
+        // TODO: Implement register
+        // Check if ustername already exist etc.
+        cookies.delete("session");
+        throw redirect(302, "/login");
+    },
+    deleteaccount: async ({ request, locals, cookies }) => {
+        // TODO: Implement delete account
+        // Check if username already exist etc.
 
-    const user = await database.user.findUnique({
-      where: { session: locals.session },
-    });
+        const user = await database.user.findUnique({
+            where: { session: locals.session },
+        });
 
-    if (user) {
-      const result = await database.user.delete({
-        where: { id: user?.id },
-      });
+        if (user) {
+            const result = await database.user.delete({
+                where: { id: user?.id },
+            });
 
-      console.log(result);
+            console.log(result);
 
-      cookies.delete("userid");
-      throw redirect(302, "/login");
-    } else {
-      return fail(404, { delete: "deletion error" });
-    }
-  },
+            cookies.delete("userid");
+            throw redirect(302, "/login");
+        } else {
+            return fail(404, { delete: "deletion error" });
+        }
+    },
 };
